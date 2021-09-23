d13fdf44de54186ed9203564747111c99c34726d
[Lampart] Tran Hoang Thang <hoang_thang@lampart-vn.com>
2021-09-23 13:56:59 +0700

    update

--- app/Services/Deploys/DeployService.php
+++ app/Services/Deploys/DeployService.php
@@ -9,14 +9,14 @@ class DeployService
     public function doDeploy($environment, $server, $version)
     {
         // todo: gọi api lên server để Deploy
-        // $result = LakaDeploy::Deploy(
-        //     $server,
-        //     $environment,
-        //     $version
-        // );
+        $result = LakaDeploy::Deploy(
+            $server,
+            $environment,
+            $version
+        );
 
-        $status = true; //$result[$environment]['status'];
-        $message = 'deploy success!'; //$result[$environment]['data']->return[0];
+        $status = $result[$environment]['status'];
+        $message = $result[$environment]['data']->return[0];
 
         return [$status, $message];
     }
