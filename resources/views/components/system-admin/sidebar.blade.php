f8d8357f1c4d805547df3a612a8e24135679653c
[Lampart] Vo Minh Tien <mintien020900@gmail.com>
2021-11-30 09:07:13 +0700

    up

--- resources/views/components/system-admin/sidebar.blade.php
+++ resources/views/components/system-admin/sidebar.blade.php
@@ -1,12 +1,55 @@
-<div class="bg-light border-right" id="sidebar-wrapper">
-    <div class="list-group list-group-flush">
+{{--<div class="bg-light border-right" id="sidebar-wrapper">--}}
+{{--    <div class="list-group list-group-flush">--}}
 
-    @foreach($LEFTMENU as $itemLeft)
-        @php($activeClass = Route::currentRouteName() == $itemLeft->route_name ? 'active font-weight-bold bg-info' : '')
-        {!! link_to_route($itemLeft->route_name, __($itemLeft->lang), [], [
-            'class' => get_classes(['list-group-item', 'list-group-item-action', $activeClass])
-        ]) !!}
-    @endforeach
+{{--    @foreach($LEFTMENU as $itemLeft)--}}
+{{--        @php($activeClass = Route::currentRouteName() == $itemLeft->route_name ? 'active font-weight-bold bg-info' : '')--}}
+{{--        {!! link_to_route($itemLeft->route_name, __($itemLeft->lang), [], [--}}
+{{--            'class' => get_classes(['list-group-item', 'list-group-item-action', $activeClass])--}}
+{{--        ]) !!}--}}
+{{--    @endforeach--}}
+{{--    </div>--}}
+{{--</div>--}}
+
+<nav id="sidebar">
+    <div class="sidebar-header">
+        <h3>Bootstrap Slider</h3>
     </div>
-</div>
+    <ul class="lisst-unstyled components">
+        <p>The Providers</p>
+        <li class="active">
+            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a
+            >
+            <ul class="collapse list-unstyled" id="homeSubmenu">
+                <li>
+                    <a href="#">Home 1</a>
+                </li>
+                <li>
+                    <a href="#">Home 2</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">About</a>
+        </li>
+        <li>
+            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a
+            >
+            <ul class="collapse list-unstyled" id="pageSubmenu">
+                <li>
+                    <a href="#">Page 1</a>
+                </li>
+                <li>
+                    <a href="#">Page 2</a>
+                </li>
+            </ul>
+        </li>
 
+        <li>
+            <a href="#">Policy</a>
+        </li>
+
+        <li>
+            <a href="#">Contact Us</a>
+        </li>
+    </ul>
+</nav>
--- resources/views/layouts/system-admin.blade.php
+++ resources/views/layouts/system-admin.blade.php
@@ -2,11 +2,11 @@
 
 @section('content_layout')
     <!-- Sidebar -->
-    <div class="col-lg-2 pr-0">
-        @include('components.system-admin.sidebar')
-    </div>
+{{--    <div class="col-lg-2 pr-0">--}}
+{{--        @include('components.system-admin.menu')--}}
+{{--    </div>--}}
     <!-- Main content -->
-    <div class="col-lg-10 px-0">
+    <div class="col-lg-12 px-0">
         <div class="card px-3">
             @yield('content')
         </div>
