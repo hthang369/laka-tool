07a888f2e3a9760b8808adfecbf3c4342c9379f5
[Lampart] Vo Minh Tien <mintien020900@gmail.com>
2022-01-04 15:51:19 +0700

    Add filter column name with screen Laka bussiness plant list

--- app/Repositories/BusinessPlans/BusinessPlanRepository.php
+++ app/Repositories/BusinessPlans/BusinessPlanRepository.php
@@ -5,6 +5,7 @@ namespace App\Repositories\BusinessPlans;
 use App\Models\BusinessPlans\BusinessPlan;
 use App\Presenters\BusinessPlans\BusinessPlanGridPresenter;
 use App\Repositories\Core\CoreRepository;
+use Laka\Core\Repositories\FilterQueryString\Filters\WhereLikeClause;
 use Lampart\Hito\Core\Repositories\FilterQueryString\Filters\WhereClause;
 
 class BusinessPlanRepository extends CoreRepository
@@ -12,7 +13,7 @@ class BusinessPlanRepository extends CoreRepository
     protected $modelClass = BusinessPlan::class;
 
     protected $filters = [
-        'name' => WhereClause::class
+        'name' => WhereLikeClause::class
     ];
 
     protected $presenterClass = BusinessPlanGridPresenter::class;
