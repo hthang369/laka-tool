826a616b5bda9eaf2c698bec3b09529c028cdb6a
[Lampart] Tran Hoang Thang <hoang_thang@lampart-vn.com>
2022-01-05 17:10:55 +0700

    update

--- app/Repositories/LakaUsers/Filters/WhereLikeClause.php
+++ app/Repositories/LakaUsers/Filters/WhereLikeClause.php
@@ -7,7 +7,7 @@ class WhereLikeClause extends BaseClause
     protected function apply($query)
     {
         return $query->filter(function($value, $key) {
-            return str_contains(strtolower(data_get($value, $this->column)), strtolower($this->values));
+            return str_contains(vn_str_filter(strtolower(data_get($value, $this->column))), vn_str_filter(strtolower($this->values)));
         });
     }
 
