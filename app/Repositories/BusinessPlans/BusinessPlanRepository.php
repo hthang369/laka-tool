dc9ab844661f0c036ebf03241889f9ca2ef4cf89
[Lampart] Vo Minh Tien <mintien020900@gmail.com>
2022-01-06 15:55:47 +0700

    update filter

--- app/Repositories/BusinessPlans/BusinessPlanRepository.php
+++ app/Repositories/BusinessPlans/BusinessPlanRepository.php
@@ -5,6 +5,7 @@ namespace App\Repositories\BusinessPlans;
 use App\Models\BusinessPlans\BusinessPlan;
 use App\Presenters\BusinessPlans\BusinessPlanGridPresenter;
 use App\Repositories\Core\CoreRepository;
+use Laka\Core\Repositories\FilterQueryString\Filters\FullTextSearchClause;
 use Laka\Core\Repositories\FilterQueryString\Filters\WhereLikeClause;
 use Lampart\Hito\Core\Repositories\FilterQueryString\Filters\WhereClause;
 
@@ -13,7 +14,7 @@ class BusinessPlanRepository extends CoreRepository
     protected $modelClass = BusinessPlan::class;
 
     protected $filters = [
-        'name' => WhereLikeClause::class
+        'name' => FullTextSearchClause::class
     ];
 
     protected $presenterClass = BusinessPlanGridPresenter::class;
