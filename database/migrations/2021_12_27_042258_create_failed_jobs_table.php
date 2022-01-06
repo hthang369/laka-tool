54ac6d4ec4e75cc0618039b68f333d7e7f88f5a1
[Lampart] Tran Hoang Thang <hoang_thang@lampart-vn.com>
2022-01-06 13:06:49 +0700

    update

--- database/migrations/2021_12_27_042258_create_failed_jobs_table.php
+++ database/migrations/2021_12_27_042258_create_failed_jobs_table.php
@@ -13,6 +13,7 @@ class CreateFailedJobsTable extends Migration
      */
     public function up()
     {
+        $this->down();
         Schema::create('failed_jobs', function (Blueprint $table) {
             $table->id();
             $table->string('uuid')->unique();
