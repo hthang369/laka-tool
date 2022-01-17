52b96e1f456f7fe3b2e24df4b6730ea031daa95e
[Lampart] Vo Minh Tien <mintien020900@gmail.com>
2022-01-17 07:59:22 +0700

    update

--- app/Presenters/LakaUsers/LakaUserGridPresenter.php
+++ app/Presenters/LakaUsers/LakaUserGridPresenter.php
@@ -30,9 +30,9 @@ class LakaUserGridPresenter extends BaseGridPresenter
                     $companyName = Company::where('id', $item['company'])->select(['name'])->first();
 
                     if (is_null($companyName)) {
-                        return '<span class="badge badge-warning">' . $item['company'] . '</span>';
+                        return  $item['company'] ;
                     } else {
-                        return '<span class="badge badge-success">' . $companyName['name'] . '</span>';
+                        return  $companyName['name'];
 
                     }
                 }
