f8076c5a1c8d328ef0ade305f1c715e36a40cf06
[Lampart] Vo Minh Tien <mintien020900@gmail.com>
2022-02-25 10:35:44 +0700

    update icon for button accept and pause

--- resources/views/components/laka-user-management/btn-accept.blade.php
+++ resources/views/components/laka-user-management/btn-accept.blade.php
@@ -10,17 +10,26 @@
             3 =>'accept',
     ];
     $arrColorBtn = [
-        1=>'btn-success',
-        2=>'btn-warning',
-        3=>'btn-success'
+        1=>'success',
+        2=>'warning',
+        3=>'success'
+    ];
+    $arrIcon = [
+        1=>'fas fa-check',
+        2=>'fas fa-pause',
+        3=>'fas fa-check'
     ];
     $urlAction = "{$sectionCode}.{$routeNameWithStatus[$data['request_approval_status']]}";
     $nameBtn = $arrNameBtn[$data['request_approval_status']];
     $colorBtn = $arrColorBtn[$data['request_approval_status']];
-
+    $iconBtn = $arrIcon[$data['request_approval_status']];
 @endphp
-<a class="btn btn-sm {{$colorBtn}}" href="{{ route($urlAction,['id'=>$data['id']])}}" onclick="return window.confirm('{{__('common.are_you_sure',['message'=>"{$nameBtn}"])}}')" >@lang("common.{$nameBtn}")</a>
-<a name="destroy" class="btn btn-sm btn-danger data-remote" href="{{ route("{$sectionCode}.delete-token",['id'=>$data['id']])}}" title="Delete" data-loading="Loading" data-trigger-confirm="1" data-confirmation-msg="Are you sure you want to delete?" data-method="DELETE" data-pjax-target="#approvalApiToken-grid">
-    <i class="fa fa fa-trash"></i>
+{!! bt_link_to_route($urlAction, __("common.$nameBtn"), $colorBtn, ['id'=>$data['id']], ['class' => 'btn-sm', 'icon' => $iconBtn,
+'onclick'=>"return window.confirm('".__('common.are_you_sure',['message'=>"{$nameBtn}"])."')"]) !!}
 
+<a name="destroy" class="btn btn-sm btn-danger data-remote"
+   href="{{ route("{$sectionCode}.delete-token",['id'=>$data['id']])}}" title="Delete" data-loading="Loading"
+   data-trigger-confirm="1" data-confirmation-msg="Are you sure you want to delete?" data-method="DELETE"
+   data-pjax-target="#approvalApiToken-grid">
+    <i class="fa fa fa-trash"></i>
 </a>
