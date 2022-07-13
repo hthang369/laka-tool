f3786ebe8d182e8fc15c56d511f2cf081372a6eb
[Lampart] Tran Hoang Thang <hoang_thang@lampart-vn.com>
2022-07-13 08:59:10 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,6 @@ if (version_compare(PHP_VERSION, '7.2.0', '>=')) {
     error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
 }
 
-// Route::get('/', function () {
-//     return redirect('system-admin/version');
-// });
-
 Route::get('/storage/files/{folder}/{name}', function ($folder, $name) {
     return Storage::disk('local')->download('public/files/' . $folder . '/' . $name);
 });
@@ -29,12 +25,6 @@ Route::group(['middleware' => ['log-activity']], function () {
     Auth::routes();
 });
 
-Route::group(['prefix' => 'system-admin', 'middleware' => ['auth:web', 'permission', 'log-activity']], function () {
-    Route::get('confirm-dialog/{id}', 'Commons\CommonController@confirmDialog')->name('common.confirm');
-    // version route
-    Route::get('version', 'Versions\VersionController@index')->name('version.index');//->middleware("log.activity:Version index");
-
-
-
-
-});
+// Route::group(['prefix' => 'system-admin', 'middleware' => ['auth:web', 'permission', 'log-activity']], function () {
+//     Route::get('confirm-dialog/{id}', 'Commons\CommonController@confirmDialog')->name('common.confirm');
+// });
